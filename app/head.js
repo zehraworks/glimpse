20d5e8563e93eb0df851ec08522fdd3b31ba5ac9
Zehra <aydin.fzehra@gmail.com>
2023-01-23 20:26:30 +0300

    add icon and update title of head element

--- app/head.js
+++ app/head.js
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title></title>
+      <title>Glimpse</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/glimpse.png" />
     </>
-  )
+  );
 }
