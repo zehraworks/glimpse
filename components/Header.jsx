cbb46e0af1b64548798dc55c6665fc8cb9b1ded7
Zehra <aydin.fzehra@gmail.com>
2023-01-28 00:47:48 +0300

    create header component

---
+++ components/Header.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../public/glimpse.png";
+
+export default function Header() {
+  return (
+    <div className="container w-2/3 h-32 flex flex-col items-center justify-center bg-slate-900 my-4 p-2 rounded-lg">
+      <Link href="/" className="flex flex-col items-center">
+        <Image alt="logo" src={logo} height={50} />
+        <h1 className="text-2xl text-slate-200">Z's Blog</h1>
+      </Link>
+      <p className="text-slate-100 italic">
+        A{" "}
+        <a className="underline decoration-dotted  decoration-sky-500">
+          glimpse
+        </a>{" "}
+        of my CS notes
+      </p>
+    </div>
+  );
+}
