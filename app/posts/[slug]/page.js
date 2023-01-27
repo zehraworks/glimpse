4301650f53162be106661753d9cc045477fb516a
Zehra <aydin.fzehra@gmail.com>
2023-01-27 19:38:38 +0300

    display post content

--- app/posts/[slug]/page.js
+++ app/posts/[slug]/page.js
@@ -1,5 +1,20 @@
-export default function PostPage() {
+import fs from "fs";
+import Markdown from "markdown-to-jsx";
+
+const getPostContent = (slug) => {
+  const folder = "posts/";
+  const file = `${folder}${slug}.md`;
+  const content = fs.readFileSync(file, "utf8");
+  return content;
+};
+
+export default function PostPage({ params }) {
+  const slug = params.slug;
+  const content = getPostContent(slug);
   return (
-    <div>PostPage</div>
-  )
+    <div>
+      <h1>PostPage {slug}</h1>
+      <Markdown>{content}</Markdown>
+    </div>
+  );
 }
