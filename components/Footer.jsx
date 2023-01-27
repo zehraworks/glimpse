6983c7e15c108e534cf2b78fe3e14ee872440393
Zehra <aydin.fzehra@gmail.com>
2023-01-28 01:23:26 +0300

    create footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+import logo from "../public/github.svg"
+
+export default function Footer() {
+  return (
+    <div className="w-2/3 h-12 flex justify-center items-center bg-slate-900 text-slate-200 my-2 rounded-lg">
+      <p>Developed by</p>
+      <Link target="_blank" href="https://github.com/aydinfz">
+        <Image className="ml-3" alt="logo" src={logo} height={40} />
+      </Link>
+    </div>
+  );
+}
