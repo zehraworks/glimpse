612a05b9c02a67099366dbb39781a578579b7582
Zehra <aydin.fzehra@gmail.com>
2023-01-27 20:01:59 +0300

    parse metadata

--- app/page.js
+++ app/page.js
@@ -1,21 +1,34 @@
 import fs from "fs";
 import Link from "next/link";
+import matter from "gray-matter";
 
 const getPostMetaData = () => {
   const folder = "posts/";
   const files = fs.readdirSync(folder);
   const markdownPosts = files.filter((file) => file.endsWith(".md"));
-  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
-  return slugs
+
+  const posts = markdownPosts.map((fileName) => {
+    const fileContent = fs.readFileSync(`posts/${fileName}`, "utf8");
+    const matterResult = matter(fileContent);
+    return {
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      subtitle: matterResult.data.subtitle,
+      slug: fileName.replace(".md", ""),
+    };
+  });
+  return posts;
 };
 
 export default function HomePage() {
   const postMetaData = getPostMetaData();
-  const postPreview = postMetaData?.map((slug) => (
+  const postPreview = postMetaData?.map((post) => (
     <div>
-      <Link href={`/posts/${slug}`}>
-        <h2>{slug}</h2>
+      <Link href={`/posts/${post.slug}`}>
+        <h2>{post.title}</h2>
       </Link>
+      <p>{post.subtitle}</p>
+      <p>{post.date}</p>
     </div>
   ));
   return <div>{postPreview}</div>;
