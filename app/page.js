386798834f1d20aabcfec31ebe41fd37e0d48706
Zehra <aydin.fzehra@gmail.com>
2023-01-27 19:07:52 +0300

    create posts pages

--- app/page.js
+++ app/page.js
@@ -1,3 +1,22 @@
+import fs from "fs";
+import Link from "next/link";
+
+const getPostMetaData = () => {
+  const folder = "posts/";
+  const files = fs.readdirSync(folder);
+  const markdownPosts = files.filter((file) => file.endsWith(".md"));
+  const slugs = markdownPosts.map((file) => file.replace(".md", ""));
+  return slugs
+};
+
 export default function HomePage() {
-    return <h1>Home Page</h1>
+  const postMetaData = getPostMetaData();
+  const postPreview = postMetaData?.map((slug) => (
+    <div>
+      <Link href={`/posts/${slug}`}>
+        <h2>{slug}</h2>
+      </Link>
+    </div>
+  ));
+  return <div>{postPreview}</div>;
 }
