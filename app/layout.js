76d16dfbb25b72324c773dd2cba3569ad9901035
Zehra <aydin.fzehra@gmail.com>
2023-01-29 10:45:32 +0300

    add next/font and update fonts

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,21 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import "../styles/globals.css";
+import { Nunito } from "@next/font/google";
+
+const font = Nunito({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <head />
-      <body className="container w-2/3 mx-auto flex flex-col items-center">
-        <Header/>
-        {children}
-        <Footer/>
-        </body>
+      <body className={font.className}>
+        <div className="container w-2/3 min-h-screen mx-auto flex flex-col items-center">
+          <Header />
+          {children}
+          <Footer />
+        </div>
+      </body>
     </html>
   );
 }
